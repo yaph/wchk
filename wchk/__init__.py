7dec2d123ba104a7bdb08d5b82e0276a703047e5
Ramiro Gómez <code@ramiro.org>
2022-09-11 23:38:09 +0200

    Move check function to __init__.py Return result in check function Output result in log function Add --output-file argument

--- wchk/__init__.py
+++ wchk/__init__.py
@@ -1,3 +1,20 @@
 # -*- coding: utf-8 -*-
 # SPDX-FileCopyrightText: 2022-present Ramiro Gómez <code@ramiro.org>
 # SPDX-License-Identifier: MIT
+import httpx
+
+
+def check(url):
+    result = {'url': url}
+
+    try:
+        resp = httpx.head(url)
+    except Exception as err:
+        result['exception'] = err
+        return result
+
+    result['status'] = resp.status_code
+    if resp.is_redirect:
+        result['redirect'] = resp.headers['location']
+
+    return result
